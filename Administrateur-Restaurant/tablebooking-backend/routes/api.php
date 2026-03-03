18de9c1db299c513749ddfb6766b22173c5e025e
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 03:21:08 +0000

    auto-commit

--- Administrateur-Restaurant/tablebooking-backend/routes/api.php
+++ Administrateur-Restaurant/tablebooking-backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
