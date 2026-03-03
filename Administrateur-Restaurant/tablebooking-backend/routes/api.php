d8dd1a8bcd8eed835121f24d98c2f64e9e263669
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 03:20:48 +0000

    auto-commit

--- Administrateur-Restaurant/tablebooking-backend/routes/api.php
+++ Administrateur-Restaurant/tablebooking-backend/routes/api.php
@@ -12,3 +12,25 @@ Route::middleware('auth:sanctum')->group(function () {
         return $request->user();
     });
 });
+
+Route::post('/register', function (Request $request) {
+
+    $request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6',
+    ]);
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'user' => $user,
+        'token' => $token
+    ]);
+});
