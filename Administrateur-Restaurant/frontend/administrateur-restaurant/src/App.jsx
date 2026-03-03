9f6d3545352c1eae1e54a76b2782fb95e3fe4e3e
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 23:10:21 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="max-w-6xl mx-auto">
-        <nav class name="flex items-center justify-between p-6 lg:px-8"{
+        <nav class name {
           constructor(parameters) {
             
           }
