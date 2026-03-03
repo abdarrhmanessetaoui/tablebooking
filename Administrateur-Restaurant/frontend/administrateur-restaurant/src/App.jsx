88959868949987a16b7d36e422d2b9ce90845638
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 23:02:56 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className="max-w-6xl mx-auto p-4">
+      <div className="max-w-6xl mx-auto">
         <h1 className="text-3xl font-bold mb-4">Administrateur Restaurant</h1>
       </div>
     </>
