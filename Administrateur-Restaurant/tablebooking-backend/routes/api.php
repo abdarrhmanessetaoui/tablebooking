8d4f0cfed90d7dfb537178cad97b5390a1d5c44e
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 03:16:09 +0000

    auto-commit

--- Administrateur-Restaurant/tablebooking-backend/routes/api.php
+++ Administrateur-Restaurant/tablebooking-backend/routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
