096ce0fdb5c65204be3a851937e356ae5035a875
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 02:49:12 +0000

    auto-commit

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Seeder;
+use App\Models\Reservation;
 use App\Models\Restaurant;
 use Illuminate\Support\Facades\File;
 
