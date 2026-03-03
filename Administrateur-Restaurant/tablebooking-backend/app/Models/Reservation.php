7e3740b1a31811fd4bb8cb2aee31771ef31e1f9a
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 05:32:20 +0000

    auto-commit

--- Administrateur-Restaurant/tablebooking-backend/app/Models/Reservation.php
+++ Administrateur-Restaurant/tablebooking-backend/app/Models/Reservation.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = [
+        'restaurant_id',
+        'client_name',
+        'client_phone',
+        'date',
+        'time',
+        'guests',
+        'status',
+    ];
+
+    protected $casts = [
+        'date' => 'date',
+        'time' => 'datetime:H:i',
+    ];
+
+    /**
+     * Get the restaurant this reservation belongs to.
+     */
+    public function restaurant(): BelongsTo
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
 }
