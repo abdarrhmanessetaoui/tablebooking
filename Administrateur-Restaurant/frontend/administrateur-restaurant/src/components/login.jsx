48751da8bd99e59a20ae22cc4206dd5ab2db42d0
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-03 23:27:22 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/login.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/login.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const login = () => {
   return (
-    <div>login</div>
+    <div class="flex justify-center items-center h-screen">
+  <div class="xl:w-[700px] px-10 h-[400px] rounded-3xl xl:shadow-xl">
+    <h1 class="text-center text-3xl font-bold mt-2 mb-2">Login</h1>
+    <hr>
+    <div class="flex justify-center mt-10">
+      <form action="">
+        <input type="text" name="" id="" class="py-3 p-5 rounded-md  bg-zinc-50 md:w-[500px] w-[300px] outline-indigo-400" placeholder="Enter your email">
+        <br><br>
+        <input type="text" name="" id="" class="py-3 p-5 rounded-md  bg-zinc-50 md:w-[500px] w-[300px] outline-indigo-400" placeholder="Enter your password">
+
+        <div class="flex justify-end mt-3 mb-4">
+          <a href="#" class="text-blue-700">Forgot password</a>
+        </div>
+
+        <button type="submit" class="py-3 bg-indigo-400 text-white w-full rounded-md font-bold">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
   )
 }
 
