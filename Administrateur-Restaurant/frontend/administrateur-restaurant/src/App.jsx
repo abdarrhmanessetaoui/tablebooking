e90c7f94ebc91c8323b79c10a3f62e9db93e7449
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 05:48:12 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -1,26 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import Dashboard from './pages/Dashboard'
-import ProtectedRoute from './components/ProtectedRoute'
 import Reservations from './pages/Reservations'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/reservations" element={
+          <ProtectedRoute>
+            <Reservations />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   )
 }
 
 export default App
-
