0c572eb04a884fe951893cc16bfe1e92307193e2
ABDERRHMAN SETTAOUI <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 08:58:43 +0000

    ttt

--- database/migrations/2026_03_02_140130_create_restaurants_table.php
+++ database/migrations/2026_03_02_140130_create_restaurants_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('blocked_dates', function (Blueprint $table) {
+        Schema::create('restaurants', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email')->unique();
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('blocked_dates');
+        Schema::dropIfExists('restaurants');
     }
 };
