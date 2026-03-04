a0be6e61150c6f4fb3ba73e79f62545cd57bc8a8
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 04:39:14 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
+import Login from './pages/Loginogin'
 import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 
