1b47caf61fc76e5fc482073cf68bbb26bb51c961
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 03:43:18 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/sidebard.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/sidebard.jsx
@@ -1,8 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import {
-  IconGrid, IconCalendar, IconUsers,
-  IconSettings, IconLogout,
-} from "./Icons.jsx";
+
 
 const NAV = [
   { to: "/dashboard",    label: "Dashboard",     Icon: IconGrid     },
