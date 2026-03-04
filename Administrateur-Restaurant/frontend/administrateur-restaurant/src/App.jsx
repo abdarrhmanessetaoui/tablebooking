33e6baeee821544ce94cd25045cf719ca7318576
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 03:06:29 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -6,9 +6,11 @@ function App() {
 
   return (
     <>
-      <div className="max-w-6xl mx-auto">
-        <h1>Administrateur Restaurant</h1>
-      </div>
+<div className="max-w-6xl mx-auto">
+  <header className="w-full flex items-center px-8 py-3" style={{ backgroundColor: '#1e1208' }}>
+    <img src="/tablebooking.png" alt="TableBooking Logo" className="h-10 object-contain" />
+  </header>
+</div>
       <div className="max-w-7xl mt-6">
         <Routes>
           <Route path="/login" element={<Login />} />
