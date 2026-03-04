738df3dbe2748ff6c2c49852591e62f41661a400
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 06:52:33 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/login'
 import Dashboard from './pages/Dashboard'
 import Reservations from './pages/Reservations'
-import ProtectedRoute from './components/ProtectedRoute'
 import BlockedDates from './pages/BlockedDates'
+import ProtectedRoute from './components/ProtectedRoute'
 import Layout from './components/Layout'
 
 function App() {
@@ -12,18 +12,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Layout>
-              <Dashboard />
-            </Layout>
-          </ProtectedRoute>
+          <ProtectedRoute><Layout><Dashboard /></Layout></ProtectedRoute>
         } />
         <Route path="/reservations" element={
-          <ProtectedRoute>
-            <Layout>
-              <Reservations />
-            </Layout>
-          </ProtectedRoute>
+          <ProtectedRoute><Layout><Reservations /></Layout></ProtectedRoute>
+        } />
+        <Route path="/blocked-dates" element={
+          <ProtectedRoute><Layout><BlockedDates /></Layout></ProtectedRoute>
         } />
       </Routes>
     </BrowserRouter>
