79afd9759bedc4d0104135794d02ac7fa69bf599
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 03:26:44 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/login.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/login.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import '../index.css'
 
 const Login = () => {
+    const navigate = useNavigate()
   const [showPassword, setShowPassword] = useState(false)
   const [rememberMe, setRememberMe] = useState(false)
 
