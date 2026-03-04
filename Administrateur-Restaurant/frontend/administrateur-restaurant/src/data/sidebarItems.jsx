3261ff10dbb194a83defdd8e88f31bd25fe965c5
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 08:26:23 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/data/sidebarItems.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/data/sidebarItems.jsx
@@ -49,9 +49,9 @@ export const navItems = [
             <line x1="18" y1="20" x2="18" y2="10"/>
             <line x1="12" y1="20" x2="12" y2="4"/>
             <line x1="6" y1="20" x2="6" y2="14"/>
-          </svg>
+          {/* </svg>
         ),
-      }
+      } */}
   ]
   
   export const LogoutIcon = () => (
