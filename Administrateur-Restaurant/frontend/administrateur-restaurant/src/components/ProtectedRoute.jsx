b25062db3c7d7726b8af3b6efff1c96823f7da54
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 05:31:36 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/ProtectedRoute.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/ProtectedRoute.jsx
@@ -1,8 +1,1 @@
-import { Navigate } from 'react-router-dom'
-import { isAuthenticated } from '../utils/auth'
-
-const ProtectedRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/" replace />
-}
-
-export default ProtectedRoute
+v
