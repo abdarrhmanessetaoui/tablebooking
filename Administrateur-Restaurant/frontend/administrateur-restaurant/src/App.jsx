8fe3a011983d22771107e1c071aa05682b56c836
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 02:43:39 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -9,6 +9,9 @@ function App() {
       <div className="max-w-6xl mx-auto">
         <h1>Administrateur Restaurant</h1>
       </div>
+      <div className="bg-blue-500 text-white text-4xl p-10">
+      Tailwind Khddam
+    </div>
       <div className="max-w-7xl mt-6">
         <Routes>
           <Route path="/login" element={<Login />} />
