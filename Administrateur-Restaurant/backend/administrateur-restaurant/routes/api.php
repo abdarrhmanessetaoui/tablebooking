dc5a02600e298b28471f0d1d682eccf2304a1556
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 05:39:13 +0000

    auto-commit

--- Administrateur-Restaurant/backend/administrateur-restaurant/routes/api.php
+++ Administrateur-Restaurant/backend/administrateur-restaurant/routes/api.php
@@ -3,11 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\ReservationController;
 
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
     ->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
-    return response()->json($request->user());
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', fn(Request $request) => response()->json($request->user()));
+    Route::apiResource('reservations', ReservationController::class);
 });
