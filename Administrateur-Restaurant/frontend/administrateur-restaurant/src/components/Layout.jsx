979d4b5179d4b6f6adf6e69d39288501a4664d80
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 08:47:23 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/Layout.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/Layout.jsx
@@ -47,7 +47,7 @@ export default function Layout({ children }) {
           </button>
         </header>
 
-        <main className="flex-1 overflow-y-auto">
+        <main className="flex-1 overflow-y-auto" >
           {children}
         </main>
 
