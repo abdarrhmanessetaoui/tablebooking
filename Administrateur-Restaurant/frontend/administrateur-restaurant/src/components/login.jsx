cf01852c07274e1bb5a2f7384a22aa8416825da0
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 02:57:00 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/login.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import  from '../index.css'
+import '../index.css'
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false)
   const [rememberMe, setRememberMe] = useState(false)
