9582e20cac0a5b5b8f6d1ae9435663424ddd55e5
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 05:31:39 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/ProtectedRoute.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/ProtectedRoute.jsx
@@ -1,1 +1,11 @@
-v
+import { Navigate } from 'react-router-dom'
+
+export default function PrivateRoute({ children }) {
+  const token = localStorage.getItem('token') || sessionStorage.getItem('token')
+
+  if (!token) {
+    return <Navigate to="/login" replace />
+  }
+
+  return children
+}
