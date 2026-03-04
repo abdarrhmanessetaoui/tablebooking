c21133e0675444bb62c846b545384dc388604fe0
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 03:06:45 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
 <div className="max-w-6xl mx-auto">
   <header className="w-full flex items-center px-8 py-3" style={{ backgroundColor: '#1e1208' }}>
-    <img src="/tablebooking.png" alt="TableBooking Logo" className="h-10 object-contain" />
+    <img src="iam/tablebooking.png" alt="TableBooking Logo" className="h-10 object-contain" />
   </header>
 </div>
       <div className="max-w-7xl mt-6">
