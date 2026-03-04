5cbd4306a015231886eb23e47e44de088e112854
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 03:24:42 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     </header>
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </>
   )
