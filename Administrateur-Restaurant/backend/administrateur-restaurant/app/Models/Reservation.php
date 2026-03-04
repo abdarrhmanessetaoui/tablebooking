12e6f702cfa27e5a8ab522fdd74d20ab8c010562
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-04 05:42:30 +0000

    auto-commit

--- Administrateur-Restaurant/backend/administrateur-restaurant/app/Models/Reservation.php
+++ Administrateur-Restaurant/backend/administrateur-restaurant/app/Models/Reservation.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'date',
+        'time',
+        'guests',
+        'status',
+        'notes',
+    ];
 }
