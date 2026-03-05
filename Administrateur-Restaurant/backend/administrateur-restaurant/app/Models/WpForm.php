e0827f7e8e8532210230086fb6162ccb5c12ffb9
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-05 00:45:37 +0000

    auto-commit

---
+++ Administrateur-Restaurant/backend/administrateur-restaurant/app/Models/WpForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WpForm extends Model
+{
+    protected $table      = 'wpjn_cpappbk_forms';
+    public    $timestamps = false;
+
+    protected $fillable   = ['defaultstatus'];
+
+    public function messages()
+    {
+        return $this->hasMany(WpMessage::class, 'formid');
+    }
+}
