0ffcdd9df98f53b931305f6772e89ce2aad1dc28
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-05 03:47:31 +0000

    auto-commit

--- Administrateur-Restaurant/backend/administrateur-restaurant/app/Http/Controllers/RestaurantReservationController.php
+++ Administrateur-Restaurant/backend/administrateur-restaurant/app/Http/Controllers/RestaurantReservationController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\WpMessage;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class RestaurantReservationController extends Controller
 {
