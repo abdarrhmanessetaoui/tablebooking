893fcff7a6c13836f44971cbb6dabc981a65a9f0
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-07 07:02:24 +0000

    auto-commit

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/Layout.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/components/Layout.jsx
@@ -108,7 +108,7 @@ export default function Layout({ children }) {
               </button> */}
 
             {/* </div> */}
-        </header> */ */}
+        </header> */ */
 
         {/* Content */}
         <main
