98b57263e51c5bacce1a40dadf9e6b0ae21d0c52
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-08 00:33:35 +0000

    auto-commit

--- Administrateur-Restaurant/backend/administrateur-restaurant/routes/console.php
+++ Administrateur-Restaurant/backend/administrateur-restaurant/routes/console.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
+Schedule::command('reservations:cancel-expired')->dailyAt('00:05');
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
