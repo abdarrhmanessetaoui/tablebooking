1fb0cc71e98ae21dcc41758c7927e5c0501952e9
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-08 00:55:05 +0000

    auto-commit

--- Administrateur-Restaurant/backend/administrateur-restaurant/routes/api.php
+++ Administrateur-Restaurant/backend/administrateur-restaurant/routes/api.php
@@ -19,10 +19,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/stats',                                      [RestaurantReservationController::class, 'stats']);
     Route::get('/reservations/by-date',                       [RestaurantReservationController::class, 'byDate']);
     Route::get('/reports',                                    [RestaurantReservationController::class, 'reports']);
+    Route::get('/restaurant/info',                            [RestaurantReservationController::class, 'info']);
     Route::get('/restaurant/reservations',                    [RestaurantReservationController::class, 'index']);
     Route::get('/restaurant/reservations/{id}',               [RestaurantReservationController::class, 'show']);
-    Route::get('/restaurant/info', [RestaurantReservationController::class, 'info']);
+    Route::post('/restaurant/reservations',                   [RestaurantReservationController::class, 'store']);
     Route::patch('/restaurant/reservations/{id}/status',      [RestaurantReservationController::class, 'updateStatus']);
+    Route::delete('/restaurant/reservations/{id}',            [RestaurantReservationController::class, 'destroy']);
     Route::post('/blocked-dates',                             [BlockedDateController::class, 'store']);
     Route::delete('/blocked-dates/{blockedDate}',             [BlockedDateController::class, 'destroy']);
     Route::put('/time-slots',                                 [TimeSlotController::class,   'update']);
