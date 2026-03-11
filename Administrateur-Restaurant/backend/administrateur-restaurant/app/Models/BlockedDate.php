2362427c10ee32e91cb4a6b47107e2fb151a1e7e
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-11 00:07:37 +0000

    auto-commit

--- Administrateur-Restaurant/backend/administrateur-restaurant/app/Models/BlockedDate.php
+++ Administrateur-Restaurant/backend/administrateur-restaurant/app/Models/BlockedDate.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class BlockedDate extends Model
 {
     protected $fillable = ['date', 'reason', 'form_id'];
+
+    protected $casts = [
+        'date' => 'date:Y-m-d',
+    ];
+
+    public function getDateAttribute($value): string
+    {
+        return $value ? \Carbon\Carbon::parse($value)->format('Y-m-d') : '';
+    }
 }
