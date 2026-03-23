3e742c2a34b7e465874d852346c79a5d1805582a
Abderrhman Settaoui <ssetaouiabdarrhmane2004@gmail.com>
2026-03-23 12:58:11 +0100

    Auto-sync: file changes

--- Administrateur-Restaurant/frontend/administrateur-restaurant/src/pages/Reservations.jsx
+++ Administrateur-Restaurant/frontend/administrateur-restaurant/src/pages/Reservations.jsx
@@ -1,4 +1,3 @@
 import ReservationsFilters from '../components/Reservations/ReservationsFilters/index.jsx'
 import ReservationsTable   from '../components/Reservations/ReservationsTable/index.jsx'
 import ReservationModal    from '../components/Reservations/ReservationModal/index.jsx'
-
